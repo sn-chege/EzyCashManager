ddb2fff962306c3635e0b00606769b969210c3c6
STANLEY NGUGI <leeblittz@gmail.com>
2024-01-30 06:58:49 +0300

    Frontend - components setup complete

---
+++ JwtAuthenticationManager/Models/LoginResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthenticationManager.Models
+{
+    public class LoginResponse
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string JwtToken { get; set; }
+
+        public int ExpiresIn { get; set; }
+
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
---
+++ JwtAuthenticationManager/Models/RegistrationRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthenticationManager.Models
+{
+    public class RegistrationRequest
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phonenumber { get; set; }
+
+        public string NationalId { get; set; }
+
+        public string Password { get; set; }
+
+        public string Salt { get; set; }
+
+        public string Role { get; set; }
+    }
+}
